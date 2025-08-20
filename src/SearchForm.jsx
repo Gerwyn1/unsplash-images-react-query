07b15037f6b65d988e4c6507e93d74f2c30b3d01
gerwyn <gerwynsim@hotmail.com>
2025-08-20 15:20:11 +0800

    set up query/render images

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,6 +1,24 @@
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+  };
   return (
-    <div>SearchForm</div>
-  )
-}
-export default SearchForm
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
+};
+export default SearchForm;
